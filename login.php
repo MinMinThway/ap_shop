0d584bfa76cc504c9ef0769e8a516f50a248d82c
MinMinThway <minthway668@example.com>
2021-05-28 12:28:45 +0630

    'auth'

---
+++ login.php
@@ -0,0 +1,184 @@
+<?php
+	session_start();
+	 require 'config/config.php';
+	 require 'config/common.php';
+
+	 if ($_POST) {
+
+	 	$email = $_POST['email'];
+	 	$password =$_POST['password'];
+	 	if (empty($_POST['email']) || empty($_POST['password']) || strlen($_POST['password']) < 4) {
+	 		if (empty($_POST['email'])) {
+	 			$emailError = "Email cannot be null";
+	 		}
+
+	 		if (empty($_POST['password'])) {
+	 			$passwordError = "Password cannot be null";
+	 		}
+
+	 		if (!empty($_POST['password']) && strlen($_POST['password'])) {
+	 			$passwordErrors = "Password should be at least 4 chars";
+	 		}
+	 	}else{
+	 		$stmt = $pdo->prepare("SELECT * FROM user WHERE email = :email");
+	 		$stmt->execute( [":email" => $email] );
+	 		$user = $stmt->fetch(PDO::FETCH_ASSOC);
+	 		if($user){
+	 			if (password_verify($password, $user['password'])) {
+	 				$_SESSION['user_id'] = $user['id'];
+	 				$_SESSION['username'] = $user['name'];
+	 				$_SESSION['logged_id'] = time();
+
+	 				header("location:index.php");
+	 			}
+	 		}
+
+	 		echo "<script> alert('Incorrect Crenditals'); </script>";
+	 	}
+	 }
+?>
+
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="img/fav.png">
+	<!-- Author Meta -->
+	<meta name="author" content="CodePixar">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>Karma Shop</title>
+
+	<!--
+		CSS
+		============================================= -->
+	<link rel="stylesheet" href="css/linearicons.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/nice-select.css">
+	<link rel="stylesheet" href="css/nouislider.min.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+
+	<!-- Start Header Area -->
+	<header class="header_area sticky-header">
+		<div class="main_menu">
+			<nav class="navbar navbar-expand-lg navbar-light main_box">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<a class="navbar-brand logo_h" href="index.html" style="height: 50px;"><h4>AP Shopping<h4></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					
+				</div>
+			</nav>
+		</div>
+		
+	</header>
+	<!-- End Header Area -->
+
+	<!-- Start Banner Area -->
+	<section class="banner-area organic-breadcrumb">
+		<div class="container">
+			<div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
+				<div class="col-first">
+					<h1>Login/Register</h1>
+					<nav class="d-flex align-items-center">
+						<a href="index.html">Home<span class="lnr lnr-arrow-right"></span></a>
+						<a href="category.html">Login/Register</a>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Banner Area -->
+
+	<!--================Login Box Area =================-->
+	<section class="login_box_area section_gap">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="login_box_img">
+						<img class="img-fluid" src="img/login.jpg" alt="">
+						<div class="hover">
+							<h4>New to our website?</h4>
+							<p>There are advances being made in science and technology everyday, and a good example of this is the</p>
+							<a class="primary-btn" href="register.php">Create an Account</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="login_form_inner">
+						<h3>Log in to enter</h3>
+						<form class="row login_form" action="login.php" method="post" id="contactForm" novalidate="novalidate">
+
+							<input name="_token" type="hidden" value="<?php echo $_SESSION['_token']; ?>">
+							<div class="col-md-12 form-group">
+								<input type="email" class="form-control" id="email" name="email" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" style="<?php if(empty($emailError)) {echo '';} else{ echo "border: 1px dotted red;"; }?>">
+							</div>
+							<div class="col-md-12 form-group">
+								<input type="password" class="form-control" id="password" name="password" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" style="<?php if(empty($passwordError)) {echo '';} else{ echo "border: 1px dotted red;"; }?>">
+								<p class="text-warning font-italic float-left "><?php if(empty($passwordError) && !empty($passwordErrors)){ echo "Password characters should be 4 chars"; }else{ echo ""; } ?></p>
+							</div>
+
+
+							<div class="col-md-12 form-group">
+								<input type="submit" name="submit" class="btn btn-warning form-control text-white" value="login" style="font-size: 20px;">
+							</div>
+							
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Login Box Area =================-->
+
+	<!-- start footer Area -->
+	<footer class="footer-area section_gap">
+		<div class="container">
+		<div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
+		  <p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+		Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+		<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+		</p>
+		</div>
+		</div>
+	</footer>
+	<!-- End footer Area -->
+
+
+	<script src="js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+	 crossorigin="anonymous"></script>
+	<script src="js/vendor/bootstrap.min.js"></script>
+	<script src="js/jquery.ajaxchimp.min.js"></script>
+	<script src="js/jquery.nice-select.min.js"></script>
+	<script src="js/jquery.sticky.js"></script>
+	<script src="js/nouislider.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<!--gmaps Js-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="js/gmaps.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+
+</html>
